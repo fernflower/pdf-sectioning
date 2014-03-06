649a332752101eaabf218b6ef70b0ef1d3faeec6
Ina Vasilevskaya <i-vasilevskaja@igrade.ru>
2014-03-06 20:06:47 +0400

    some tests for add mark

--- tests/bookcontrollertest.py
+++ tests/bookcontrollertest.py
@@ -11,7 +11,7 @@ class MocTocController(object):
     def get_elem(self, cas_id, mode):
         return None
 
-    def set_state(self, value, cas_id):
+    def set_state(self, value, cas_id, is_ok=False, braces_err=False):
         pass
 
     def process_zone_added(self, zone):
@@ -21,6 +21,7 @@ class MocTocController(object):
 # an object representing mark but without all QWidget stuff
 class MockMark(object):
     def __init__(self, *args, **kwargs):
+        self.pos = kwargs.get("pos")
         self.corrections = kwargs.get("corrections") or (0, 0)
         self.corrected = False
         self.is_selected = False
@@ -30,11 +31,15 @@ class MockMark(object):
         self.pages = kwargs.get("pages") or [self.page]
         self.cas_id = kwargs.get("cas_id")
         self.delete_func = kwargs.get("delete_func") or None
-        self.type = ""
+        self.type = kwargs.get("type")
 
     @property
     def name(self):
         return "mock object (%s): %s" % (self.type, self.cas_id)
+
+    def y(self):
+        x, y = self.pos
+        return y
 
     def show(self):
         return "shown!"
@@ -48,6 +53,21 @@ class MockMark(object):
     def should_show(self, page):
         return True
 
+    def is_paragraph(self):
+        return self.type != "ruler"
+
+    def is_start(self):
+        return self.type == "start"
+
+    def is_end(self):
+        return self.type == "end"
+
+    def is_zone(self):
+        return self.type == "zone"
+
+    def is_passthrough_zone(self):
+        return self.pass_through
+
 
 class MockMarkCreator(object):
     def _create_mark(self, *args, **kwargs):
@@ -55,7 +75,6 @@ class MockMarkCreator(object):
 
     def make_paragraph_mark(self, *args, **kwargs):
         mark = self._create_mark(*args, **kwargs)
-        mark.type = "paragraph"
         return mark
 
     def make_ruler_mark(self, *args, **kwargs):
@@ -90,6 +109,76 @@ class DocLoaderTest(unittest.TestCase):
         self.controller.load_markup(self.markup_file, "MockParent")
         # 16 paragraphs with data and 2 dataless ones in the beginning
         self.assertEqual(len(self.controller.paragraph_marks.keys()), 18)
+
+    def test_add_marks(self):
+        # zone addition: test that proper keys appear in
+        # paragraphs\parapgraph_marks and that behaviour is ok for both
+        # pass_through and non-pass through zones
+        # Pass through zones should appear on every paragraph page
+
+        # mind that zone marks can't be added unless start AND end marks exist.
+        # If any mark (start\end) has been deleted, then all zones are deleted
+        # as well
+
+        self.assertTrue(self.controller.paragraphs == {})
+        self.assertTrue(self.controller.paragraph_marks == {})
+        start_data = {"pos": (0, 10),
+                      "parent": "MockParent",
+                      "cas_id": "lesson:bla-bla-bla",
+                      "page": 5,
+                      "delete_func": lambda x: x,
+                      "name": "lesson blablabla. Paragraph 2",
+                      "type": "start"}
+        start = self.controller.add_mark(start_data)
+        self.assertTrue(start.page, 5)
+        self.assertTrue(start.cas_id in self.controller.paragraph_marks)
+        self.assertEqual(self.controller.paragraph_marks[start.cas_id],
+                         {"marks": (start, None), "zones": []})
+        # check that mark is in paragraphs
+        self.assertTrue(start.page in self.controller.paragraphs)
+        self.assertEquals(self.controller.paragraphs[start.page],
+                          {"marks": [start], "zones": []})
+        # now add end mark
+        end_data = {"pos": (0, 190),
+                    "parent": "MockParent",
+                    "cas_id": "lesson:bla-bla-bla",
+                    "page": 25,
+                    "delete_func": lambda x: x,
+                    "name": "lesson blablabla. Paragraph 2",
+                    "type": "end"}
+
+        end = self.controller.add_mark(end_data)
+        self.assertTrue(end.page, 25)
+        self.assertTrue(end.cas_id in self.controller.paragraph_marks)
+        self.assertEqual(self.controller.paragraph_marks[end.cas_id],
+                         {"marks": (start, end), "zones": []})
+        # check that mark is in paragraphs
+        self.assertTrue(end.page in self.controller.paragraphs)
+        self.assertEquals(self.controller.paragraphs[end.page],
+                          {"marks": [end], "zones": []})
+        # try to add duplicate end mark, make sure that nothing has changed
+        m = self.controller.add_mark(end_data)
+        self.assertEqual(m, None)
+        self.assertEqual(self.controller.paragraph_marks[end.cas_id],
+                         {"marks": (start, end), "zones": []})
+
+        # TODO continue with add zone tests
+        zone_data = {"pos": (45, 20),
+                     "parent": "MockParent",
+                     "cas_id": "lesson:bla-bla-bla",
+                     "zone_id": "01int",
+                     "page": 12,
+                     "delete_func": lambda x: x,
+                     "objects":[ {"oid": "195-12-01-01-int",
+                                  "block-id": "some block id"}],
+                     "number": 1,
+                     "rubric": "int",
+                     "margin": "l",
+                     "pass_through": False}
+        zone = self.controller.add_zone(zone_data)
+        self.assertTrue(zone.page in self.controller.paragraphs)
+        self.assertEqual(self.controller.paragraphs[zone.page],
+                         {"marks": [], "zones": [zone]})
 
 
 if __name__ == '__main__':
